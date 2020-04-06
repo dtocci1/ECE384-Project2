1f7ce34eefc3e7c6820fb5df12ca0a21874826c4
dtocci1 <43154755+dtocci1@users.noreply.github.com>
2020-04-06 14:12:25 -0700

    Added project file

---
+++ Project5_2.m
@@ -0,0 +1,19 @@
+%% ECE 384 - Project 5.2
+%% Tyler Botelho, Lewis Brum, Matt LaVoie, Dylan Tocci
+
+% 1. Generate sequences, at least 10000 points long, of the following two
+% random variables:
+
+% (a) A uniform random variable X1 in the range of [0 3]
+
+
+% (b) A uniform random variable X2 in the range of [0 5].
+
+
+% 2. Obtain a new random variable as a sum: Y = X1 + X2.
+
+
+% 3. Measure the PDFs of X1, X2 and Y .
+
+
+% 4. Plot the measured PDFs along with the corresponding theoretical PDFs. Compare and comment on the results.
